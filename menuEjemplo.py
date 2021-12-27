c5656197a6f65a61b211ba56a5ab2eba4eb9de03
sanchezantelo <sofiasa@live.com.ar>
2021-12-27 17:18:41 +0000

    se agrega ejemplo de menu en python

---
+++ menuEjemplo.py
@@ -0,0 +1,18 @@
+
+while True: 
+    print("Menu Opciones:")
+    print("1. Ver lista de alumnos")
+    print("2. Añadir alumno")
+    print("3. Salir")
+    print("")
+    op= input("Ingrese una opcion:")
+    
+    if op == "1":
+        print("Ver lista")
+        print("")
+    if op == "2":
+        print("Añadir alumno")
+        print("")
+    if op == "3":
+        print("salir")
+        break
