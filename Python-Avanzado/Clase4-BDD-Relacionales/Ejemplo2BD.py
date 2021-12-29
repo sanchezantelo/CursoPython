57943923b9dc753784d05a47123ad83902e5af79
sanchezantelo <sofiasa@live.com.ar>
2021-12-29 18:03:48 +0000

    modificaciones, uso de base de datos online

---
+++ Python-Avanzado/Clase4-BDD-Relacionales/Ejemplo2BD.py
@@ -0,0 +1,17 @@
+import pymysql
+
+personas=(("Oscar",30),("Ernesto",41),("Osvaldo",27))
+
+conn=pymysql.connect(host="db4free.net",port=3306,user="sofiasa",passwd="Sanchez0503",db="bddsanchez")
+
+cursor=conn.cursor()
+
+idi=0
+for nombre,edad in personas:
+    cursor.execute("INSERT INTO Personas VALUES (%s,%s,%s)",(idi,nombre,edad))
+    idi+1
+
+conn.commit()
+conn.close()
+
+print("Datos ingresados correctamente!")
