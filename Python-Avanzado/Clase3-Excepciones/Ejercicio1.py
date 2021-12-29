eddf12b84b82d2f6b73b0dad49795e7cab9f7996
sanchezantelo <sofiasa@live.com.ar>
2021-12-29 12:59:53 +0000

    Practica archivo txt y excel

--- Python-Avanzado/Clase3-Excepciones/Ejercicio1.py
+++ Python-Avanzado/Clase3-Excepciones/Ejercicio1.py
@@ -1,5 +1,6 @@
-import biblioteca as bib
 import os
+import time
+from openpyxl import Workbook
 
 def pedido():
     nombre = input("Ingrese el nombre del cliente: ")
@@ -36,10 +37,34 @@ def calculo(lista):
     print("Debe abonar: $", total)
     return total
 
+#Funcion para guardar archivo
+def guardar(lista, costo):
+    archivo = time.asctime()
+    archivo = archivo.replace(":",".")
+    archivo = archivo.replace(" ","")
+    f = open(archivo+".txt", "w")
+    f.write("El cliente:"+lista[0]+"\n")
+    f.write("Empanadas de carne: "+str(lista[1])+"\n")
+    f.write("Empanadas de jamon y queso:: "+str(lista[2])+"\n")
+    f.write("Empanadas de humita:  "+str(lista[3])+"\n")
+    f.write("Empanadas de atun: "+str(lista[4])+"\n")
+    f.write("Importe abonado: "+str(costo)+"\n")
+    f.write("Pedido procesado por : "+empleado)
+    f.close()
+
+def guardarexcel(lista,costo):
+    nueva = lista + [costo] + [time.asctime()]
+    ws1.append(nueva)
+
+wb= Workbook()
+ws1= wb.active
+ws1.append(["Nombre","Ec","Ejyq","Eh","Ea","Costo","Fecha"])
+
 print("Bienvenido")
 empleado= input("Ingrese su nombre: ")
 precio = input("Ingrese precio unitario: ")
 precio = entero(precio)
+
 while True:
     print("""
     1 - Nuevo pedido
@@ -50,7 +75,10 @@ while True:
         listap = pedido()
         print(listap)
         importe= calculo(listap)
+        #guardar(listap,importe)
+        guardarexcel(listap,importe)
         input("Presione enter para continuar...")
         os.system("cls")
     if opcion == "2" :
+        wb.save(filename="empanadas.xlsx")
         break
