6084a2eb95e3b40b49f48adc6f5e821772a4aceb
sanchezantelo <sofiasa@live.com.ar>
2021-12-29 18:39:26 +0000

    modificacion id

--- Python-Avanzado/Clase4-BDD-Relacionales/Ejemplo2BD.py
+++ Python-Avanzado/Clase4-BDD-Relacionales/Ejemplo2BD.py
@@ -6,10 +6,10 @@ conn=pymysql.connect(host="db4free.net",port=3306,user="sofiasa",passwd="Sanchez
 
 cursor=conn.cursor()
 
-idi=0
+idi=1
 for nombre,edad in personas:
     cursor.execute("INSERT INTO Personas VALUES (%s,%s,%s)",(idi,nombre,edad))
-    idi+1
+    idi= idi+1
 
 conn.commit()
 conn.close()
